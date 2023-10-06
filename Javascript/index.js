99ef432c0b775af46aaf6c9826d1601785c549be
Cole Smith <coletosmitty@gmail.com>
2023-10-05 22:42:31 -0600

    css hover fix

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,11 +3,5 @@ const hoverText = document.getElementById('hover-text');
 
 image.addEventListener('click', function () {
     
-    hoverText.style.display = 'none';
-    
-    // Toggle the 'zoomed' class to apply the zoom effect
-    image.classList.toggle('zoomed');
-    setTimeout(function () {
-        window.location.href = "login.html";
-    }, 1000);
+    window.location.href = "login.html";
 });
