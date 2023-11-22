6ffd7fb16f3eb4273e7a4bc14db700c04f1458ba
Cole Smith <coletosmitty@gmail.com>
2023-11-22 00:22:07 -0700

    fixes to click count service

--- Javascript/backend.js
+++ Javascript/backend.js
@@ -8,6 +8,19 @@ const PORT = process.env.PORT || 3000;
 const drinkButtons = {
     drinkButton1: { clickCount: 0 },
     drinkButton2: { clickCount: 0 },
+    drinkButton3: { clickCount: 0 },
+    drinkButton4: { clickCount: 0 },
+    drinkButton5: { clickCount: 0 },
+    drinkButton6: { clickCount: 0 },
+    drinkButton7: { clickCount: 0 },
+    drinkButton8: { clickCount: 0 },
+    drinkButton9: { clickCount: 0 },
+    drinkButton10: { clickCount: 0 },
+    drinkButton11: { clickCount: 0 },
+    drinkButton12: { clickCount: 0 },
+    drinkButton13: { clickCount: 0 },
+    drinkButton14: { clickCount: 0 },
+    drinkButton15: { clickCount: 0 },
   };
   
 
