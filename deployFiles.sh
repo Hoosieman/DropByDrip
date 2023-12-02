b2a354b4e4be6849cbc3322e9e077dbe61f71fba
Cole Smith <coletosmitty@gmail.com>
2023-12-02 16:09:40 -0700

    as

--- deployFiles.sh
+++ deployFiles.sh
@@ -27,7 +27,8 @@ ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/$service/public
+rsync -r -u -e "ssh -i $key" * ubuntu@$hostname:services/$service/public
+
 
 
 ###
